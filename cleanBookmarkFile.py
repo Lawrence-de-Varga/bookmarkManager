8e0fbabb814bdc94a9642f329d2edf9a51d2f4a7
Lawrence de Varga <lawrencedevarga@gmail.com>
2024-07-25 21:38:01 +1000

    cleanup
    turned into function and cleaned up

--- cleanBookmarkFile.py
+++ cleanBookmarkFile.py
@@ -4,13 +4,14 @@
 # This greatly eases the job of manipulating the bookmarks file.
 # The resulting file is still readable by the browser (at least on chrome and brave)
 
-with open('bookmarks_7_25_24.html', 'r') as f:
-    f_contents = f.readlines()
-    with open('rearranged_bookmarks.html', 'w') as nf:
-        for line in f_contents:
-            if '<DT>' in line:
-                new_line = line.rstrip() + '</DT>\n'
-                nf.write(new_line)
-            else:
-                nf.write(line)
-                
+def cleanfile(file):
+    with open(file, 'r') as f:
+        f_contents = f.readlines()
+        with open('rearranged_bookmarks.html', 'w') as nf:
+            for line in f_contents:
+                if '<DT>' in line:
+                    new_line = line.rstrip() + '</DT>\n'
+                    nf.write(new_line)
+                else:
+                    nf.write(line)
+        return 'rearranged_bookmarks.html'
