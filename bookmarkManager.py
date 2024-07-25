a80da0b0c74e6af9d1f7d0e1962b0258fd66acff
Lawrence de Varga <lawrencedevarga@gmail.com>
2024-07-25 21:41:01 +1000

    tidying
    cleaned up old defunct structures. now printing what I think will be a usable structure.

--- bookmarkManager.py
+++ bookmarkManager.py
@@ -1,45 +1,26 @@
 import bs4
 from bs4 import BeautifulSoup
-import pprint
+from cleanBookmarkFile import cleanfile
 
-with open('smallbookmarks.html') as html_file:
+line = '----------------------------------------------------------------------'
+
+with open(cleanfile('testbookmarks.html')) as html_file:
     soup = BeautifulSoup(html_file, 'lxml')
 
 
 # base is the only key in the first level of
-# the bookmarks dict. It's value is a list 
-# containg all the top level folders and bookmarks 
+# the bookmarks dict. It's value is a list
+# containg all the top level folders and bookmarks
 base = soup.dl.dt
 
 sl = soup.dl.dl
 
-folders = []
+
+def printsl() -> None:
+    for item in sl.find_all(['a', 'dt', 'dl'], recursive=False):
+        print(type(item))
+        print(item.prettify())
+        print(line)
 
 
-for item in sl.find_all('dl', recursive=False):
-    folders.append(item.text)
-
-bookmarks_dict = {}
-for item in sl.find_all('dl', recursive=False):
-    bookmarks_dict[item.previous_sibling.text.rstrip()] = item
-
-
-def links_list(bs_result: bs4.element.ResultSet) -> list[bs4.element.ResultSet]:
-    return [item.find_all('a') for item in bs_result if item.a]
-    
-def printBookmarks(bookmark_dict: dict) -> None:
-    for key, value in bookmark_dict.items():
-        print(key)
-        print()
-        print(value)
-        print()
-        print()
-        print('-----------------------------------------------------------------------------------')
-
-# printBookmarks(bookmarks_dict)
-
-q = links_list(sl.find_all('dt', recursive=False))
- 
-
-for item in q:
-    print(item)
+printsl()
