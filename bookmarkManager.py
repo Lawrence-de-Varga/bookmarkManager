ece42964dbf3db99b0faee11208b3d0532bea9f3
Lawrence de Varga <lawrencedevarga@gmail.com>
2024-07-26 17:29:50 +1000

    collectItems
    Beginning of the logic to collect the html objects into a dict which will be easier to manipulate.

--- bookmarkManager.py
+++ bookmarkManager.py
@@ -16,32 +16,63 @@ base = soup.dl.dt
 sl = soup.dl.dl
 
 
-def isLink(tag: bs4.element.tag) -> bool:
-    if tag.find('a', recursive=False):
+def isLink(item: bs4.element.Tag) -> bool:
+    if item.find('a', recursive=False):
         return True
     else:
         return False
 
 
-def isFolderTitle(tag: bs4.element.tag) -> bool:
-    if tag.find('h3', recursive=False):
+def isFolderTitle(item: bs4.element.Tag) -> bool:
+    if item.find('h3', recursive=False):
         return True
     else:
         return False
 
 
-def isFolderList(tag: bs4.element.tag) -> bool:
-    if (not isLink(tag) and not isFolderTitle(tag)):
+def isFolderList(item: bs4.element.Tag) -> bool:
+    if (not isLink(item) and not isFolderTitle(item)):
         return True
     else:
         return False
 
 
-def printsl() -> None:
-    for item in sl.find_all(['a', 'dt', 'dl'], recursive=False):
-        print(type(item))
-        print(item.prettify())
-        print(line)
+bookmarksDict = {}
+bookmarksDict[base] = []
 
 
-printsl()
+def collectItens(bms: bs4.element.ResultSet) -> dict:
+    previous_tag = ''
+    index = 0
+
+    for tag in bms:
+        if isLink(tag):
+            bookmarksDict[base].append(tag)
+            previous_tag = tag
+            index += 1
+        elif isFolderTitle(tag):
+            bookmarksDict[base].append({tag: ['testing']})
+            previous_tag = tag
+            index += 1
+        else:
+            previous_tag = tag
+            index += 1
+    return bookmarksDict
+
+
+bms = sl.find_all(['a', 'dt', 'dl'], recursive=False)
+
+collectItens(bms)
+
+for item in bookmarksDict[base]:
+    print(item)
+    print(line)
+
+# def printsl() -> None:
+#     for item in sl.find_all(['a', 'dt', 'dl'], recursive=False):
+#         print(type(item))
+#         print(item.prettify())
+#         print(line)
+
+
+# printsl()
