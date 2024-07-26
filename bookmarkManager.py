74c7c94e7d978f2c53d77cdf9ae4c1ed6b95ccd2
Lawrence de Varga <lawrencedevarga@gmail.com>
2024-07-26 23:26:16 +1000

    collecItems recursion
    collectItems is now fully functional. i.e it recurrs down the folders to create a tree of bookmark links and folders

--- bookmarkManager.py
+++ bookmarkManager.py
@@ -2,8 +2,13 @@ import bs4
 from bs4 import BeautifulSoup
 from cleanBookmarkFile import cleanfile
 
+# just here for testing to seperate various print statements
 line = '----------------------------------------------------------------------'
 
+# The bookmarks.html file produced by chrome and brave (and any others
+# using the same fomat) does not have closing </DT> tags.
+# This makes parsing the file extremely clumsy
+# so the file is first modified to add the </DT> tags
 with open(cleanfile('testbookmarks.html')) as html_file:
     soup = BeautifulSoup(html_file, 'lxml')
 
@@ -11,9 +16,10 @@ with open(cleanfile('testbookmarks.html')) as html_file:
 # base is the only key in the first level of
 # the bookmarks dict. It's value is a list
 # containg all the top level folders and bookmarks
-base = soup.dl.dt
+base: bs4.element.Tag = soup.dl.dt
 
-sl = soup.dl.dl
+# soup.dl.dl skips past the header which is not needed
+sl: bs4.element.Tag = soup.dl.dl
 
 
 def isLink(item: bs4.element.Tag) -> bool:
@@ -41,38 +47,42 @@ bookmarksDict = {}
 bookmarksDict[base] = []
 
 
-def collectItens(bms: bs4.element.ResultSet) -> dict:
+def collectItems(bms: bs4.element.ResultSet, root: bs4.element.Tag, bmsDict: dict) -> dict:
     previous_tag = ''
     index = 0
 
     for tag in bms:
+        # if the tag is just a link we simply put it in the list
         if isLink(tag):
-            print("LINK")
-            print(index)
-            print(tag)
-            print(line)
-            bookmarksDict[base].append(tag)
+            bmsDict[root].append(tag)
             previous_tag = tag
             index += 1
+
+        # if the tag is a folder title we construct an empty dict which will be
+        # subsequently filled with more links and titles and folders
         elif isFolderTitle(tag):
-            print("TITLE")
-            print(index)
-            print(tag)
-            print(line)
-            bookmarksDict[base].append({tag: ['testing']})
+            bmsDict[root].append({tag: []})
             previous_tag = tag
             index += 1
+
+        # collectItems calls itself to contrusct a tree out of the bookmark folders
         elif isFolderList(tag):
-            print("LIST")
-            print(index)
-            print(tag)
-            print(bookmarksDict[base][index - 1])
-            print(line)
-            bookmarksDict[base][index - 1][previous_tag] = tag
+            newResultSet = tag.find_all(['a', 'dt', 'dl'], recursive=False)
+            bmsDict[root][index - 1][previous_tag] = collectItems(newResultSet, previous_tag, bmsDict[root][index - 1])
             previous_tag = tag
-    return bookmarksDict
+    return bmsDict[root]
 
 
-bms = sl.find_all(['a', 'dt', 'dl'], recursive=False)
+# This takes the sl tag and strips out the <p> and \n components which litter
+# the file.
+bmResultSet: bs4.element.ResultSet = sl.find_all(['a', 'dt', 'dl'], recursive=False)
 
-collectItens(bms)
+bookmarksDict[base] = collectItems(bmResultSet, base, bookmarksDict)
+
+# Just for testing
+for item in bookmarksDict[base]:
+    if item is dict:
+        for key, value in item.items():
+            print(key)
+            print(value)
+            print(line)
