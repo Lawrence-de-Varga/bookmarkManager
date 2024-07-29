6ec8384099dc4e5b9c3839191bad752545e3d690
Lawrence de Varga <lawrencedevarga@gmail.com>
2024-07-29 19:26:26 +1000

    typos.

--- processBmDict.py
+++ processBmDict.py
@@ -1,9 +1,10 @@
 import bs4
 from bookmarkManager import bookmarksDict, base, line
+import copy
 
 # The bookmarksDict is not manipulated as that would mess with the index
 # so a copy is made and then modified as we traverse down bmDict
-bmDictCp = bookmarksDict.deepcopy()
+bmDictCp = copy.deepcopy(bookmarksDict)
 
 
 def print_link(link: bs4.element.Tag) -> None:
@@ -21,10 +22,12 @@ def find_link_path(link: bs4.element.Tag, bmdict: dict) -> list:
     index = 0
     for item in bmdict[root]:
         if item == link:
+            breakpoint()
             path.append(bmdict[root].index(item))
             return path
         elif isinstance(item, dict):
             possible_path = find_link_path(link, item)
+            breakpoint()
             if isinstance(possible_path[-1], int):
                 ind = bmdict[root].index(item)
                 path.append(ind)
@@ -44,34 +47,6 @@ def delete_link(link: bs4.element.Tag, path: list, bmdict: dict) -> dict:
         bmdict.remove(link)
     else:
         delete_link(link, path[1:], bmdict[path[0]])
-    
-
-    
-# def process_link(link: bs4.element.Tag, current_location: dict, current_location_key: bs4.element.Tag) -> dict:
-#     print_link(link)
-#     action = input("Where would you like this bookmark to be stored? \n(Hit enter to leave it in the current location.)\n")
-#     if action == '':
-#         print("You hit enter to leave the link where it is.")
-#         print(line)
-#         return current_location
-#     else:
-#         print(f"You entererd: {action}.")
-#         print(line)
-#         return current_location
-
-
-# def processBmDict(bmdict: dict, root: bs4.element.Tag) -> dict:
-#     index = 0
-#     for item in bmdict[root]:
-#         print(type(item))
-#         if isinstance(item, dict):
-#             print(extract_key(item).text)
-#             print(line)
-#             bmdict[root][index] = processBmDict(item, extract_key(item))
-#             index += 1
-#         else:
-#             bmdict[root][index] = process_link(item, bmdict, root)
-#             index += 1
 
 
 # processBmDict(bookmarksDict, base)
