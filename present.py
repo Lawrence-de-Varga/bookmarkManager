81a4240ca6ade34340c10091bbf91cf3fb62fe8d
Lawrence de Varga <lawrencedevarga@gmail.com>
2024-08-13 22:00:25 +1000

    Added map_folders/links_to_tags functions.

--- present.py
+++ present.py
@@ -1,3 +1,4 @@
+import bs4
 from typeAliases import bmbf_list
 from printing import get_link_href, folder_name
 
@@ -10,3 +11,10 @@ def make_folders_presentable(folders_list: bmbf_list) -> list[str]:
     return list(map(folder_name, folders_list))
 
 
+def map_links_to_tags(links_list: list[str], link_tag_list: bmbf_list) -> dict[str, bs4.element.Tag]:
+    return dict(zip(links_list, link_tag_list))
+
+
+def map_folders_to_tags(folders_list: list[str], folder_tag_list: bmbf_list) -> dict[str, bs4.element.Tag]:
+    return dict(zip(folders_list, folder_tag_list))
+    
