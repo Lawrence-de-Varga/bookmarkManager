1767c98cd148ed30f6debcc65ed4abae75aad7ae
Lawrence de Varga <lawrencedevarga@gmail.com>
2024-08-13 20:09:12 +1000

    collect_folders function added.

--- collect.py
+++ collect.py
@@ -1,12 +1,17 @@
 import bs4
-from htmlToDict import bmdict, bmlist
+from paths import extract_key
+from htmlToDict import bmdict, bmlist, bookmarksDict
 
 bmbf_list = list[bs4.element.Tag]
 
 
-def collect_folders_h(bmsdict: bmdict, folders_list: bmbf_list) -> bmbf_list:
-    for item in bmsdict:
+def collect_folders_h(bmsdict: bmdict, folders_list: bmlist) -> bmlist:
+    root = extract_key(bmsdict)
+    folders_list.append(root)
+    for item in bmsdict[root]:
         if isinstance(item, dict): 
+            folders_list.extend(collect_folders_h(item, []))
+    return folders_list
 
 
 def collect_folders(bmsdict: bmdict) -> bmbf_list:
