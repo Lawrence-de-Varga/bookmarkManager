3734081712b0184232d431d621faae1fd24ccdcf
Lawrence de Varga <lawrencedevarga@gmail.com>
2024-08-21 19:43:42 +1000

    Swaped out map calls for list comprehensions.

--- present.py
+++ present.py
@@ -7,13 +7,13 @@ from printing import get_link_href, folder_name
 def make_links_presentable(links_list: bmbf_list) -> list[str]:
     """Takes a list of link bs4 tags and returns a list containing just the
     url of the tag."""
-    return list(map(get_link_href, links_list))
+    return [get_link_href(link) for link in links_list]
 
 
 def make_folders_presentable(folders_list: bmbf_list) -> list[str]:
     """Takes a list of folderr bs4 tags and returns a list
     containing just the folder titles."""
-    return list(map(folder_name, folders_list))
+    return [folder_name(folder) for folder in folders_list]
 
 
 def map_links_to_tags(links_list: list[str], link_tag_list: bmbf_list) -> dict[str, bs4.element.Tag]:
@@ -24,15 +24,12 @@ def map_folders_to_tags(folders_list: list[str], folder_tag_list: bmbf_list) -> 
     return dict(zip(folders_list, folder_tag_list))
     
 
-# def present_folders(folders_list: list[str]) -> None:
-#     for folder in folders_list:
-#         print(folder)
-
 def find_longest(item_list: list[str]) -> int:
     return len(sorted(item_list, key=len)[-1])
 
 
 def present_folders_h(folders_list: list[str], num_per_line: int, start: int, stop: int, index: int) -> None:
+    """Presents all of the bookmark folder titles in an indexed grid."""
     length_of_longest = find_longest(folders_list) + len(str(len(folders_list))) + 5
     if start >= len(folders_list):
         return
