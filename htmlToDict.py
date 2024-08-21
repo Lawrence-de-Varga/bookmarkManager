a81681a820ff862b3c1e67b31944e2b21b7eccc4
Lawrence de Varga <lawrencedevarga@gmail.com>
2024-08-21 21:12:39 +1000

    Slight changes to htmlToDict.

--- htmlToDict.py
+++ htmlToDict.py
@@ -22,13 +22,15 @@ with open(cleanfile('sampleBookmarks/smallbookmarks.html')) as html_file:
     soup = BeautifulSoup(html_file, 'lxml')
 
 
-# base is the only key in the first level of
-# the bookmarks dict. It's value is a list
+# base is a <dt> tag which will be used as a key 
+# in the bookmarks dict. It's value is a list
 # containg all the top level folders and bookmarks
 base: bs4.element.Tag = soup.dl.dt
 
 # soup.dl.dl skips past the header which is not needed
-sl: bs4.element.Tag = soup.dl.dl
+# main_html is the structure of foldes and bookmarks
+# which we operate on.
+main_html: bs4.element.Tag = soup.dl.dl
 
 
 def isLink(item: bs4.element.Tag) -> bool:
@@ -58,42 +60,34 @@ bookmarksDict[base] = []
 
 # Takes the bookmarks htmk file from the browser once modified by cleanBookmarksFile
 # and puts the links into lists as a value of a dict the key of which is the bookmark folder
-def collectItems(bms: bs4.element.ResultSet, root: bs4.element.Tag, bmsDict: bmdict) -> bmdict:
+def collectItems(bms: bs4.element.ResultSet, root: bs4.element.Tag, bmsdict: bmdict) -> bmdict:
     previous_tag = ''
     index = 0
 
     for tag in bms:
         # if the tag is just a link we simply put it in the list
         if isLink(tag):
-            bmsDict[root].append(tag)
+            bmsdict[root].append(tag)
             previous_tag = tag
             index += 1
 
         # if the tag is a folder title we construct an empty dict which will be
         # subsequently filled with more links and titles and folders
         elif isFolderTitle(tag):
-            bmsDict[root].append({tag: []})
+            bmsdict[root].append({tag: []})
             previous_tag = tag
             index += 1
 
         # collectItems calls itself to contrusct a tree out of the bookmark folders
         elif isFolderList(tag):
             newResultSet = tag.find_all(['a', 'dt', 'dl'], recursive=False)
-            bmsDict[root][index - 1][previous_tag] = collectItems(newResultSet, previous_tag, bmsDict[root][index - 1])
+            bmsdict[root][index - 1][previous_tag] = collectItems(newResultSet, previous_tag, bmsdict[root][index - 1])
             previous_tag = tag
-    return bmsDict[root]
+    return bmsdict[root]
 
 
 # This takes the sl tag and strips out the <p> and \n components which litter
 # the file.
-bmResultSet: bs4.element.ResultSet = sl.find_all(['a', 'dt', 'dl'], recursive=False)
+bmResultSet: bs4.element.ResultSet = main_html.find_all(['a', 'dt', 'dl'], recursive=False)
 
 bookmarksDict[base] = collectItems(bmResultSet, base, bookmarksDict)
-
-# Just for testing
-# for item in bookmarksDict[base]:
-#     if item is dict:
-#         for key, value in item.items():
-#             print(key)
-#             print(value)
-#             print(line)
